8fc85603304b95d4da0e95a0fa9d646ba2d7860c
confused-Techie <dev@lhbasics.com>
2022-03-16 21:15:52 -0700

    Created Script for Dev Service Icons

---
+++ devserviceIcons-Scripts/bin/deviconscrapper.js
@@ -0,0 +1,3 @@
+#!/usr/bin/env node
+
+module.exports = require("../src/cli/index.js").run(process.argv.slice(2));
---
+++ devserviceIcons-Scripts/src/cli/index.js
@@ -0,0 +1,41 @@
+const axios = require("axios");
+const fs = require('fs');
+
+async function run(rawArguments) {
+  //first lets get the devicons file
+
+  let res = await axios.get('https://raw.githubusercontent.com/devicons/devicon/master/devicon.json');
+
+  var locArray = [];
+
+  for (var i = 0; i < res.data.length; i++) {
+
+    for (var y = 0; y < res.data[i].versions.svg.length; y++) {
+
+      var itemName = `${res.data[i].name}-${res.data[i].versions.svg[y]}`;
+      var itemLink = `https://cdn.jsdelivr.net/gh/devicons/devicon/icons/${res.data[i].name}/${itemName}.svg`;
+      var itemAlt = res.data[i].name;
+
+      var itemToAdd = {
+        loc: itemLink,
+        id: itemName,
+        alt: itemAlt
+      };
+
+      locArray.push(itemToAdd);
+    }
+  }
+
+  fs.writeFile("devservices.json", JSON.stringify(locArray, null, "\t"), function(err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+
+  console.log('File Updated.');
+
+}
+
+module.exports = {
+  run,
+};
